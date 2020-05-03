1f4637dfb5e0bf6f8e9e9b72d7ad56270f085866
Manjeet <manjeet@enterprisebot.org>
2020-05-03 10:54:18 +0530

    add day 1 and day 2 problem and solution

---
+++ src/week1/first_bad_version.js
@@ -0,0 +1,42 @@
+// ******       problem statement       *******
+
+/* You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API. */
+
+// ******       SOLUTION        *******
+
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let l = 0;
+        let h = n;
+        let mid = 0;
+        while(l<=h) {
+            mid = parseInt(l + (h-l)/2);
+            if(!isBadVersion(mid)) l = mid+1;
+            else h = mid-1;
+        }
+        if(!isBadVersion(mid)) return mid+1;
+        return mid;
+    };
+};
